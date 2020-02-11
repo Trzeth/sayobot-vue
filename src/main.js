1eb9d16f4c3c0020223c55bf510b3144767904c9
Trzeth <Trzeth@Outlook.com>
2020-02-12 07:51:35 +0800

    Add vuetify

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import _ from "loadsh";
 import "./assets/style/iconfont.css";
 import "./assets/style/style.scss";
 import router from "./router";
+import vuetify from './plugins/vuetify';
 
 Vue.config.productionTip = false;
 Vue.prototype._ = _;
@@ -14,5 +15,6 @@ Vue.use(VueLocalStorage, {
 
 new Vue({
   router,
+  vuetify,
   render: h => h(App)
 }).$mount("#app");
