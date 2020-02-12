c46af3f61242666b3220537992bac800659687a0
Trzeth <Trzeth@Outlook.com>
2020-02-12 08:02:31 +0800

    sayobot -> osu.sh

--- src/components/popupViews/Setting.vue
+++ src/components/popupViews/Setting.vue
@@ -1,46 +1,88 @@
 <template>
-  <div class="setting-warpper">
-    <div class="setting" v-on:click.stop>
-      <div class="scroll" ref="scroll">
-        <div>
-          <div class="setting-page">
-            <h2>设置</h2>
-            <div class="selection">
-              <input type="checkbox" v-model="isUnicode" id="is-unicode-checkbox" />
-              <label class="title" for="is-unicode-checkbox">使用 Unicode 标题</label>
-              <span class="note">当标题为日文时 不再使用罗马音显示</span>
-            </div>
-            <div class="selection">
-              <input type="checkbox" v-model="useCDN" id="default-server-checkbox" />
-              <label class="title" for="default-server-checkbox">使用CDN加速</label>
-              <span class="note">如果下载速度还不错请不要开启这个选项，可能存在过时的旧图</span>
-            </div>
-            <div class="selection">
-              <input type="checkbox" disabled v-model="isAutoSelet" id="is-auto-select-checkbox" />
-              <label class="title" for="is-auto-select-checkbox">自动选择最适合难度</label>
-              <span class="note">打开图谱面板后，符合条件的图谱</span>
-            </div>
-            <div class="selection">
-              <label class="title" for="is-auto-select-checkbox">调整预览音频声音</label>
-              <span class="note">调整后将在网站全局生效</span>
-              <range-slider class="slider" min="0" max="1" step="0.01" v-model="volume"></range-slider>
-            </div>
-          </div>
+	<div class="setting-warpper">
+		<div class="setting" v-on:click.stop>
+			<div class="scroll" ref="scroll">
+				<div>
+					<div class="setting-page">
+						<h2>设置</h2>
+						<div class="selection">
+							<input
+								type="checkbox"
+								v-model="isUnicode"
+								id="is-unicode-checkbox"
+							/>
+							<label class="title" for="is-unicode-checkbox"
+								>使用 Unicode 标题</label
+							>
+							<span class="note"
+								>当标题为日文时 不再使用罗马音显示</span
+							>
+						</div>
+						<div class="selection">
+							<input
+								type="checkbox"
+								v-model="useCDN"
+								id="default-server-checkbox"
+							/>
+							<label class="title" for="default-server-checkbox"
+								>使用CDN加速</label
+							>
+							<span class="note"
+								>如果下载速度还不错请不要开启这个选项，可能存在过时的旧图</span
+							>
+						</div>
+						<div class="selection">
+							<input
+								type="checkbox"
+								disabled
+								v-model="isAutoSelet"
+								id="is-auto-select-checkbox"
+							/>
+							<label class="title" for="is-auto-select-checkbox"
+								>自动选择最适合难度</label
+							>
+							<span class="note"
+								>打开图谱面板后，符合条件的图谱</span
+							>
+						</div>
+						<div class="selection">
+							<label class="title" for="is-auto-select-checkbox"
+								>调整预览音频声音</label
+							>
+							<span class="note">调整后将在网站全局生效</span>
+							<range-slider
+								class="slider"
+								min="0"
+								max="1"
+								step="0.01"
+								v-model="volume"
+							></range-slider>
+						</div>
+					</div>
 
-          <div class="about-page">
-            <h2>关于</h2>
+					<div class="about-page">
+						<h2>关于</h2>
 
-            <p>Sayobot.cn是全球最大、地图存储量最多（超过2000GB）的第三方osu!镜像站， 致力于为广大玩家提供osu!周边的服务，包括但不限于osu!地图的搜索下载、提供osu!外设（小键盘和数位板）、图包&皮肤分享/下载、osu! QQ机器人等。本网站由_BlackC（夜麻麻）和 SoulDee & Trzeth 等人共同维护（SoulDee & Trzeth：_BlackC你的API又双叒叕炸掉了）</p>
-          </div>
-          <h2>Alpha Test Version -- Start at 2019.8</h2>
-          <h2>
-            Source:
-            <a href="https://github.com/Trzeth/sayobot-vue">@Trzeth/sayobot-vue</a>
-          </h2>
-        </div>
-      </div>
-    </div>
-  </div>
+						<p>
+							osu.sh
+							是全球最大、地图存储量最多（超过2000GB）的第三方osu!镜像站，
+							致力于为广大玩家提供osu!周边的服务，包括但不限于osu!地图的搜索下载、提供osu!外设（小键盘和数位板）、图包&皮肤分享/下载、osu!
+							QQ机器人等。本网站由_BlackC（夜麻麻）和 SoulDee &
+							Trzeth 等人共同维护（SoulDee &
+							Trzeth：_BlackC你的API又双叒叕炸掉了）
+						</p>
+					</div>
+					<h2>Alpha Test Version -- Start at 2019.8</h2>
+					<h2>
+						Source:
+						<a href="https://github.com/Trzeth/sayobot-vue"
+							>@Trzeth/sayobot-vue</a
+						>
+					</h2>
+				</div>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
@@ -52,102 +94,102 @@ import "vue-range-slider/dist/vue-range-slider.css";
 BScroll.use(MouseWheel);
 
 export default {
-  name: "setting",
-  localStorage: {
-    isUnicode: {
-      type: Boolean,
-      default: false
-    },
-    isAutoSelet: {
-      type: Boolean,
-      default: false
-    },
-    useCDN: {
-      type: Boolean,
-      default: false
-    },
-    volume: {
-      type: Number,
-      default: 1.0
-    }
-  },
-  components: {
-    RangeSlider
-  },
-  mounted: function() {
-    this.init();
-  },
+	name: "setting",
+	localStorage: {
+		isUnicode: {
+			type: Boolean,
+			default: false
+		},
+		isAutoSelet: {
+			type: Boolean,
+			default: false
+		},
+		useCDN: {
+			type: Boolean,
+			default: false
+		},
+		volume: {
+			type: Number,
+			default: 1.0
+		}
+	},
+	components: {
+		RangeSlider
+	},
+	mounted: function() {
+		this.init();
+	},
 
-  methods: {
-    init: function() {
-      this.bs = new BScroll(this.$refs.scroll, {
-        scrollY: true,
-        click: true,
-        probeType: 3,
-        mouseWheel: {
-          speed: 0,
-          invert: false,
-          easeTime: 300
-        }
-      });
-    }
-  }
+	methods: {
+		init: function() {
+			this.bs = new BScroll(this.$refs.scroll, {
+				scrollY: true,
+				click: true,
+				probeType: 3,
+				mouseWheel: {
+					speed: 0,
+					invert: false,
+					easeTime: 300
+				}
+			});
+		}
+	}
 };
 </script>
 
 <style lang="scss">
 .setting-warpper {
-  width: 50%;
-  min-width: 450px;
-  padding: 15px;
-  height: 100%;
-  box-sizing: border-box;
+	width: 50%;
+	min-width: 450px;
+	padding: 15px;
+	height: 100%;
+	box-sizing: border-box;
 }
 .setting {
-  height: 100%;
-  overflow: hidden;
-  border-radius: 25px;
-  background: #ffffff;
+	height: 100%;
+	overflow: hidden;
+	border-radius: 25px;
+	background: #ffffff;
 
-  .scroll {
-    height: 100%;
+	.scroll {
+		height: 100%;
 
-    > div {
-      padding: 20px;
-    }
-    h2 {
-      font-size: 1.5rem;
-      font-weight: 400;
-    }
-    .selection {
-      margin: 10px 0 0 5px;
-      padding-left: 20px;
+		> div {
+			padding: 20px;
+		}
+		h2 {
+			font-size: 1.5rem;
+			font-weight: 400;
+		}
+		.selection {
+			margin: 10px 0 0 5px;
+			padding-left: 20px;
 
-      input[type="checkbox"] {
-        float: left;
-        margin: 5px 0 0 -20px;
-      }
-      .title {
-        font-size: 1.2rem;
-        font-weight: 500;
-      }
-      .note {
-        font-size: 1rem;
-        font-weight: 500;
-        margin-top: 2px;
-        display: block;
-      }
-    }
+			input[type="checkbox"] {
+				float: left;
+				margin: 5px 0 0 -20px;
+			}
+			.title {
+				font-size: 1.2rem;
+				font-weight: 500;
+			}
+			.note {
+				font-size: 1rem;
+				font-weight: 500;
+				margin-top: 2px;
+				display: block;
+			}
+		}
 
-    .about-page {
-      margin-top: 20px;
+		.about-page {
+			margin-top: 20px;
 
-      p {
-        margin-top: 10px;
-        text-indent: 2rem;
-        line-height: 2rem;
-      }
-    }
-  }
+			p {
+				margin-top: 10px;
+				text-indent: 2rem;
+				line-height: 2rem;
+			}
+		}
+	}
 }
 </style>
--- src/views/Package.vue
+++ src/views/Package.vue
@@ -1,90 +1,92 @@
 <template>
-  <div class="title-warpper">
-    <div>
-      <div class="keep-out">
-        <div class="line"></div>
-        <h2>Under Construction</h2>
-        <div class="line"></div>
-      </div>
-      <div class="title">
-        <h1>Sayobot</h1>
-        <h2>
-          osu!
-          <router-link class="package-link" to="/home"># 图包站</router-link>
-        </h2>
-      </div>
-    </div>
-  </div>
+	<div class="title-warpper">
+		<div>
+			<div class="keep-out">
+				<div class="line"></div>
+				<h2>Under Construction</h2>
+				<div class="line"></div>
+			</div>
+			<div class="title">
+				<h1>osu.sh</h1>
+				<h2>
+					osu!
+					<router-link class="package-link" to="/home"
+						># 图包站</router-link
+					>
+				</h2>
+			</div>
+		</div>
+	</div>
 </template>
 <script>
 export default {
-  name: "package"
+	name: "package"
 };
 </script>
 
 <style lang="scss" scoped>
 .title-warpper {
-  display: flex;
-  align-items: center;
-  position: fixed;
-  top: 0;
-  bottom: 0;
-  left: 0;
-  right: 0;
-  text-align: center;
+	display: flex;
+	align-items: center;
+	position: fixed;
+	top: 0;
+	bottom: 0;
+	left: 0;
+	right: 0;
+	text-align: center;
 
-  > div {
-    flex: 1;
-  }
-  .keep-out {
-    margin-bottom: 30px;
-    .line {
-      width: 120%;
-      margin-left: -10%;
-      transform: skewX(-45deg);
+	> div {
+		flex: 1;
+	}
+	.keep-out {
+		margin-bottom: 30px;
+		.line {
+			width: 120%;
+			margin-left: -10%;
+			transform: skewX(-45deg);
 
-      border-top: 20px dashed yellow;
-      background: black;
-    }
-    h2 {
-      background: yellow;
-      margin: 0;
-      padding: 10px 0;
-      font-size: 3rem;
-      font-weight: 500;
-    }
-  }
-  .title {
-    h1 {
-      font-size: 3.2rem;
-      font-weight: 400;
-      letter-spacing: 2px;
-    }
-    h2 {
-      margin: 15px 10px 20px 0;
-      letter-spacing: 1px;
-      font-size: 2.5rem;
-      font-weight: 300;
-      color: #909090;
+			border-top: 20px dashed yellow;
+			background: black;
+		}
+		h2 {
+			background: yellow;
+			margin: 0;
+			padding: 10px 0;
+			font-size: 3rem;
+			font-weight: 500;
+		}
+	}
+	.title {
+		h1 {
+			font-size: 3.2rem;
+			font-weight: 400;
+			letter-spacing: 2px;
+		}
+		h2 {
+			margin: 15px 10px 20px 0;
+			letter-spacing: 1px;
+			font-size: 2.5rem;
+			font-weight: 300;
+			color: #909090;
 
-      .package-link {
-        font-weight: 400;
-        display: inline-block;
-        color: #909090;
+			.package-link {
+				font-weight: 400;
+				display: inline-block;
+				color: #909090;
 
-        text-decoration: none;
-        padding: 0 15px;
-        transition: all 0.5s cubic-bezier(0.075, 0.82, 0.165, 1);
-        border-radius: 0;
-      }
-      .package-link:visited {
-        color: #909090;
-      }
-      .package-link:hover {
-        color: #8f8f8f;
-        background: #e5e5e5;
-      }
-    }
-  }
+				text-decoration: none;
+				padding: 0 15px;
+				transition: all 0.5s cubic-bezier(0.075, 0.82, 0.165, 1);
+				border-radius: 0;
+			}
+			.package-link:visited {
+				color: #909090;
+			}
+			.package-link:hover {
+				color: #8f8f8f;
+				background: #e5e5e5;
+			}
+		}
+	}
 }
 </style>
